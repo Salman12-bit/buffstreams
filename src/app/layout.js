b3315f69f361a5bb962cee9b12ed0ce7c2cabf21
M Salman <salman39707@gmail.com>
2025-12-15 10:29:26 +0500

    first upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
+import Script from "next/script";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,6 +16,12 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="google-site-verification" content="oMZXDI1wVbxlIA9uS9BtkM9sorkjaO68P_GXgLyBJYI" />
         <link rel="icon" type="image/png" href="/favicon.webp" />
+
+        <Script
+          src="//d1zhmd1pxxxajf.cloudfront.net/?dmhzd=1230539"
+          strategy="afterInteractive"
+          data-cfasync="false"
+        />
       </head>
 
       <body className={inter.className}>
